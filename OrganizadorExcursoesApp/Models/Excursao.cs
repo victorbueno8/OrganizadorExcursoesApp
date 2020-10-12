28a2c234249c25f9b0270fdd546f6309cf7ea606
Victor Bueno <v.bueno@aluno.ifsp.edu.br>
2020-10-12 00:00:27 -0300

    Adicionado Excursao model e Firebase service CRUD

---
+++ OrganizadorExcursoesApp/Models/Excursao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrganizadorExcursoesApp.Models
+{
+    class Excursao
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public String LocalDestino { get; set; }
+        public String Data { get; set; }
+        public String LocalSaida { get; set; }
+        public String HorarioSaida { get; set; }
+
+    }
+}
---
+++ OrganizadorExcursoesApp/Services/FirebaseService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+using Firebase.Database.Query;
+using OrganizadorExcursoesApp.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrganizadorExcursoesApp.Services
+{
+    class FirebaseService
+    {
+        FirebaseClient firebase = new FirebaseClient("https://organizadorexcursoesapp.firebaseio.com/");
+
+        public async Task<List<Excursao>> GetExcursoes()
+        {
+            return (await firebase.Child("Excursoes").OnceAsync<Excursao>())
+                .Select(item => new Excursao
+                {
+                    Id = item.Object.Id,
+                    Nome = item.Object.Nome,
+                    Descricao = item.Object.Descricao,
+                    LocalDestino = item.Object.LocalDestino,
+                    Data = item.Object.Data,
+                    LocalSaida = item.Object.LocalSaida,
+                    HorarioSaida = item.Object.HorarioSaida
+                }).ToList();
+        }
+
+        public async Task CreateExcursao(int id, string nome, string descricao, string localdestino, string data, string localsaida, string horariosaida)
+        {
+            await firebase.Child("Excursoes").PostAsync(new Excursao()
+            {
+                Id = id,
+                Nome = nome,
+                Descricao = descricao,
+                LocalDestino = localdestino,
+                Data = data,
+                LocalSaida = localsaida,
+                HorarioSaida = horariosaida
+            });
+        }
+
+        public async Task<Excursao> GetExcursao(int id)
+        {
+            var allExcursoes = await GetExcursoes();
+
+            return allExcursoes.Where(a => a.Id == id).FirstOrDefault();
+        }
+
+        public async Task UpdateExcursao(int id, string nome, string descricao, string localdestino, string data, string localsaida, string horariosaida)
+        {
+            var toUpdateExcursao = (await firebase.Child("Excursoes").OnceAsync<Excursao>()).Where(a => a.Object.Id == id).FirstOrDefault();
+
+            await firebase.Child("Excursoes").Child(toUpdateExcursao.Key).PutAsync(new Excursao()
+            {
+                Id = id,
+                Nome = nome,
+                Descricao = descricao,
+                LocalDestino = localdestino,
+                Data = data,
+                LocalSaida = localsaida,
+                HorarioSaida = horariosaida
+            });
+        }
+
+        public async Task DeleteExcursao(int id)
+        {
+            var toDeleteExcursao = (await firebase.Child("Excursoes").OnceAsync<Excursao>()).Where(a => a.Object.Id == id).FirstOrDefault();
+
+            await firebase.Child("Excursoes").Child(toDeleteExcursao.Key).DeleteAsync();
+        }
+    }
+}
